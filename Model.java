f92ec9de046c8d7fdd84cc11f169a15d949964ed
CJenkins1212 <145162494+CJenkins1212@users.noreply.github.com>
2024-10-13 17:14:51 -0400

    Add files via upload

---
+++ Model.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Model {
+    //Initializes assorted objects
+    List<Object> People = new ArrayList<>();
+    ArrayList<Object> Person = new ArrayList<>();
+    List<Object> Hold;
+    int n = 0;
+    int g = 0;
+    int l = 0;
+    String r;
+    String s;
+    Scanner myObj = new Scanner(System.in);
+    String one;
+    String two;
+    int three;
+    float four;
+    float five;
+    float six;
+    String et;
+    String eo;
+    String ei;
+    String es;
+    int gfs;
+}
